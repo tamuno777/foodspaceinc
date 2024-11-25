357e62ad992e463d07209c865b09fe3949f47086
tamuno777 <davisbeulah07@gmail.com>
2024-11-25 15:04:36 +0100

    env

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBx-K6IMGmabN1_zOTvkw4pXoDuTQ8f2XI",
-  authDomain: "foodspace-264ce.firebaseapp.com",
-  projectId: "foodspace-264ce",
-  storageBucket: "foodspace-264ce.appspot.com",
-  messagingSenderId: "819781105751",
-  appId: "1:819781105751:web:b02edd4d56ed54c3f84b12",
-  measurementId: "G-YFGV9JW652"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
